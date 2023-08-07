df19dcc91a77173bd5f631ae2f1063a556ef4f2a
Titus Lagat <lagattyto@gmail.com>
2023-08-07 16:35:55 +0300

    Save books to local storage

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const getBooksFromLocalStorage = () => {
   const books = localStorage.getItem('Books');
   return books ? JSON.parse(books) : [];
 };
+
+const saveBooksTolocalStorage = (books) => {
+  localStorage.setItem('Books', JSON.stringify(books));
+};
