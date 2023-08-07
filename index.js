d5dd56c21aaab97bb20b84c7f7f0870479808bd2
Titus Lagat <lagattyto@gmail.com>
2023-08-07 17:15:20 +0300

    Add new book to the collection

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const saveBooksTolocalStorage = (books) => {
 };
 
 function CreateBookObject(title, author) {
-	this.title = title;
-	this.author = author;
+  this.title = title;
+  this.author = author;
 }
+
+const addBooks = () => {
+  const bookTitle = document.getElementById('title-input').value.trim();
+  const bookAuthor = document.getElementById('author-input').value.trim();
+
+  if (bookTitle && bookAuthor) {
+    const books = getBooksFromLocalStorage();
+    const book = new CreateBookObject(bookTitle, bookAuthor);
+    books.unshift(book);
+    saveBooksTolocalStorage(books);
+
+    document.getElementById('title-input').value = '';
+    document.getElementById('author-input').value = '';
+  }
+};
