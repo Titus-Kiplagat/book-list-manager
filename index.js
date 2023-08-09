0f24072e18de11f0db96c25f9a9a993401ead173
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-09 22:36:06 +0500

    Install luxon dependency library

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import addRemoveTableBorder from './modules/addRemoveTableBorder.js';
-import bookManager from './modules/bookManager.js';
-import handleNavLinkClick from './modules/handleNavLinkClick.js';
+import addRemoveTableBorder from "./modules/addRemoveTableBorder.js";
+import bookManager from "./modules/bookManager.js";
+import handleNavLinkClick from "./modules/handleNavLinkClick.js";
+import displayDateAndTime from "./modules/displayDateAndTime.js";
 
 addRemoveTableBorder();
-document.addEventListener('DOMContentLoaded', () => handleNavLinkClick());
+document.addEventListener("DOMContentLoaded", () => handleNavLinkClick());
 bookManager.handleFormSubmit();
 bookManager.handleAddButtonClick();
 bookManager.renderBooks();
+displayDateAndTime();
