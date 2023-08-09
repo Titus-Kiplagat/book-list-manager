58565e31df9e2e524692f34ee90bc489531ba55f
Hamza Baig <mail.hamzabaig@gmail.com>
2023-08-09 22:42:24 +0500

    Fix linter errors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import addRemoveTableBorder from "./modules/addRemoveTableBorder.js";
-import bookManager from "./modules/bookManager.js";
-import handleNavLinkClick from "./modules/handleNavLinkClick.js";
-import displayDateAndTime from "./modules/displayDateAndTime.js";
+import addRemoveTableBorder from './modules/addRemoveTableBorder.js';
+import bookManager from './modules/bookManager.js';
+import handleNavLinkClick from './modules/handleNavLinkClick.js';
+import displayDateAndTime from './modules/displayDateAndTime.js';
 
 addRemoveTableBorder();
-document.addEventListener("DOMContentLoaded", () => handleNavLinkClick());
+document.addEventListener('DOMContentLoaded', () => handleNavLinkClick());
 bookManager.handleFormSubmit();
 bookManager.handleAddButtonClick();
 bookManager.renderBooks();
--- modules/displayDateAndTime.js
+++ modules/displayDateAndTime.js
@@ -1,4 +1,5 @@
-import { DateTime } from "../node_modules/luxon/build/es6/luxon.js";
+import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
+
 const getCurrentDateTime = () => {
   const currentDate = DateTime.local();
   const date = {
@@ -6,9 +7,9 @@ const getCurrentDateTime = () => {
     month: currentDate.monthLong,
     day: currentDate.day,
     hour: currentDate.hour > 12 ? currentDate.hour - 12 : currentDate.hour,
-    minute: currentDate.minute.toString().padStart(2, "0"),
-    second: currentDate.second.toString().padStart(2, "0"),
-    amPm: currentDate.hour >= 12 ? "PM" : "AM",
+    minute: currentDate.minute.toString().padStart(2, '0'),
+    second: currentDate.second.toString().padStart(2, '0'),
+    amPm: currentDate.hour >= 12 ? 'PM' : 'AM',
   };
   return date;
 };
@@ -32,7 +33,7 @@ const formatDateTime = (date) => {
   return `${date.month} ${dayOrdinal} ${date.year}, ${date.hour}:${date.minute}:${date.second} ${date.amPm}`;
 };
 const displayDateAndTime = () => {
-  const dateDiv = document.querySelector(".date");
+  const dateDiv = document.querySelector('.date');
   const currentDateTime = getCurrentDateTime();
   const formattedDateTime = formatDateTime(currentDateTime);
   dateDiv.textContent = formattedDateTime;
